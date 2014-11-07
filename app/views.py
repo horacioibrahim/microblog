7cb6b79a0e653fe03ea1d3712a1b4bbe4913bc74
Horacio Ibrahim <horacioibrahim@gmail.com>
2014-11-07 16:18:18 -0200

    Mega tutorial Parte II

--- app/views.py
+++ app/views.py
@@ -3,10 +3,17 @@
 Este módulo contém as views (ou manipuladores) que respondem
 as requisições HTTP.
 """
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Olá, Mundo!"
+	user = {'nickname': 'Miguel'} # fake user
+	posts = [{'author': {'nickname':'John'}, 'body': 'Um belo dia em Brasilia'},
+	{'author': {'nickname': 'Susan'}, 'body': 'Os trapalhoes foi legal!'}]
+
+	return render_template('index.html', title='Home', user=user, posts=posts)
+			
+    		
 
